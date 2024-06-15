a35c4350a69621fe7aaedcbf0754568c35fb8aca
Leon Lucic <llucic@vub.hr>
2024-06-15 14:41:25 +0200

    dodana abstraktna klasa Student

---
+++ src/main/java/Student.java
@@ -0,0 +1,24 @@
+import org.example.studomat.Person;
+
+public class Student extends Person {
+    public Student(String name, String surname, String OIB,String JMBAG, String username, String password) throws Exception {
+        super(name, surname, OIB, username, password);
+        setJMBAG(JMBAG);
+    }
+
+    public String getJMBAG() {
+        return JMBAG;
+    }
+
+    public void setJMBAG(String JMBAG) throws Exception {
+        if(JMBAG.length()==10 && JMBAG.matches("[0-9]+")) {
+            this.JMBAG = JMBAG;
+        }else{
+            throw new Exception("Invalid JMBAG");
+        }
+    }
+
+    private String JMBAG;
+
+
+}
--- src/main/java/org/example/studomat/Person.java
+++ src/main/java/org/example/studomat/Person.java
@@ -1,10 +1,12 @@
 package org.example.studomat;
 
-abstract class Person {
-    public Person(String name, String surname, String OIB) {
+public abstract class Person {
+    public Person(String name, String surname, String OIB, String username, String password) throws Exception {
         this.name = name;
         this.surname = surname;
-        this.OIB = OIB;
+        setOIB(OIB);
+        this.username = username;
+        this.password = password;
     }
 
     public String getName() {
@@ -27,11 +29,33 @@ abstract class Person {
         return OIB;
     }
 
-    public void setOIB(String OIB) {
-        this.OIB = OIB;
+    public void setOIB(String OIB) throws Exception {
+        if(OIB.length()==11 && OIB.matches("[0-9]+")) {
+            this.OIB = OIB;
+        }else{
+            throw new Exception("Invalid OIB");
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     private String name;
     private String surname;
     private String OIB;
+    private String username;
+    private String password;
 }
